b4a5c1a8e1b4b5464088908eeba66ef5ecb242ea
Maria Sahno <sahnomyu@m2.ru>
2024-07-29 23:46:53 +0600

    webinar 10

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int idGenerator = 1;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        log.info("Пришел запрос на создание пользователя с email = {}", user.getEmail());
+        validate(user);
+        user.setId(idGenerator++);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    private void validate(User user) {
+        if (user.getName() == null || user.getName().isEmpty()) {
+            throw new RuntimeException("Имя пользователя должно быть задано!");
+        }
+    }
+}
